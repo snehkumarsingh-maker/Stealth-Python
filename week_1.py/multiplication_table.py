5aee2a15d4b92ce864f4ef307b4ae1e1a5351eea
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-08-17 18:08:25 +0530

    multiplication

---
+++ week_1.py/multiplication_table.py
@@ -0,0 +1,5 @@
+i = input("What table would you like to display ? ")
+t = int(i)
+
+for i in range (10) :
+    print (t,"X",i, "=" ,t*i)
---
+++ week_1.py/while.py
@@ -0,0 +1,9 @@
+print("Hello, everyone we are starting ")
+n = 1
+c = 1 
+while(c==1):
+    print ("The token number is ",n, " may please come in")
+    c = int(input("continue(0/1): "))
+    d = int(c)
+    n += 1
+    print ("Thank you, this is end of our day")
