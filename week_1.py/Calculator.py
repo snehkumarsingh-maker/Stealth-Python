ba25910633028ed0a3c8596a55af0b0869cc6cf6
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-07 23:59:25 +0530

    Calculator

---
+++ week_1.py/Calculator.py
@@ -0,0 +1,15 @@
+i = int(input("Enter the 1st value: "))
+j = int(input("Enter the 2nd value: "))
+
+Op = input("Operator: ")
+
+if Op == '+':
+    print("Answer= ", i +j)
+elif  Op == '+':
+    print("Answer= ", i +j)
+elif  Op == '+':
+    print("Answer= ", i +j)
+elif  Op == '+':
+    print("Answer= ", i +j)
+else:
+    print("Galat operator")
