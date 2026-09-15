8606c91a9f870cf3f48b5c628ae0a71c99dcdfd6
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-15 15:20:20 +0530

    Basic whiel loop practice

---
+++ week_1.py/While_loop.py
@@ -0,0 +1,40 @@
+
+i = 1 
+while i <= 10:
+    print(i)
+    i += 1 
+
+i = 1 
+while i <= 10:
+    if i % 2 == 0:
+        print(i)
+    i += 1 
+
+i = 1 
+while i <=10:
+    if i % 2 != 0:
+        print(i)
+    i += 1 
+
+i = 1 
+while i <= 10:
+
+    print(10-i)
+    i += 1 
+
+i = 1 
+sum = 0
+while i <= 10:
+    sum = sum + i
+    print(sum)
+    i += 1 
+
+i = 1 
+while i <= 10:
+    print(10, " * ", i, " = ", 10 * i)
+    i += 1
+    
+    
+    
+
+
