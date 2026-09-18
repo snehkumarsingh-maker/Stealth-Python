f4a936a69cd6b6abffc251cf5210c0eb2d755f6a
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-18 11:25:11 +0530

    Today class work

--- week_1.py/PRACTICE.py
+++ week_1.py/PRACTICE.py
@@ -1,14 +1,12 @@
 
 #count even number present in 1 to 50
-cout = 1
-for i in range (1,50):
-    if i % 2 == 0:
-        cout += 1
+cout = 0
+for i in range (1,50,2):
+    cout += 1
 print(cout)
 
 # count odd number present in 1 to 50 
 odd = 0
-for i in range(1,50):
-    if i % 2 != 0:
-        odd += 1
-print(odd)        
+for i in range(1,50,2):
+    odd += 1
+print(odd) 
