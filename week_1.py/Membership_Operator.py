b2f7d2075aa8214546b84f8cb1bc8f0e934ace63
Sneh Kumar Singh <snehkumarsingh78@gmail.com>
2026-09-25 23:37:36 +0530

    Today Work

---
+++ week_1.py/Membership_Operator.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+print("banana" in fruits)
+
+#Check if "pineapple" is NOT present in a list:
+
+fruits = ["apple", "banana", "cherry"]
+
+print("pineapple" not in fruits)
